9409a016194daceb7e98569bdcb04fff56a6f311
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2022-11-14 23:19:56 +0100

    Finish exo 6

--- Exo6.php
+++ Exo6.php
@@ -1,16 +1,17 @@
-<h1>Exercice6</h1>
+<h1>Exercice 6</h1>
 <p>Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau de valeurs.</p>
 
 <?php
-$elements = array("Monsieur","Madame","Mademoiselle");
-alimenterListeDeroulante($elements);
+$civilite = array("Monsieur","Madame","Mademoiselle");
+$statut=array("salarié", "étudiant", "chomeur");
+
+alimenterListeDeroulante($civilite);
+alimenterListeDeroulante($statut);
 
 function alimenterListeDeroulante($tableau){
-    $result= "<form action='action.php' method='post'>";
-    foreach ($tableau as $input){
-        $result.="<p>$input :<input type 'text' name='$input' /></p>";
+    echo '<select>';
+    foreach($tableau as $input){
+    echo '<option value=' .$input . '>' . $input . '</option>';
     }
-    $result.="<p><input type='submit' value ='OK'></p>";
-    return $result;
+    echo '</select>';
 }
-echo alimenterListeDeroulante($tableau);
